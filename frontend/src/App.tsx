c8be6f1bf02c9c6893b8d0b9d55015f9900c3c0c
pirana3 <pirana356@gmail.com>
2025-02-07 17:49:01 -0600

    added the react routert to app and made home the main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import Nav from './components/Nav.tsx'
 import Footer from './components/Footer.tsx'
 import {Routes, Route, Router} from 'react-router-dom'
+import Home from './pages/Home.tsx'
+import About from './pages/About.tsx'
+import Contact from './pages/Contact.tsx'
+import Skills from './pages/Skills.tsx'
+import Works from './pages/Works.tsx'
 
 const App = () => {
 
@@ -11,7 +16,14 @@ const App = () => {
 
       <Nav/>
       <section> 
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/About' element={<About/>} />
+          <Route path='/Contact' element= {<Contact/>} />
+          <Route path='/Skills' element= {<Skills/>} />
+          <Route path='/Works' element= {<Works/>} /> 
 
+        </Routes>
 
       </section>
 
