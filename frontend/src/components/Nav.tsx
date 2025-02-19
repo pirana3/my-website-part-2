fbb826b26fbfadafe23c18c66f93124953144701
pirana3 <pirana356@gmail.com>
2025-02-19 02:39:22 -0600

    nav stuff

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,5 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, useNavigate, Link } from 'react-router-dom';
+import {useWindowScroll} from "react-use";
+import {useEffect, useRef, useState } from "react";
+
+const navItems = ["Home ", "About", "Skills", "Works", "Contact"];
+
+const navBar = () => {
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [isIndicatorActive, setIsIndicatorActive] = useState(false);
+
+  const audioElementRef = useRef(null);
+  const vaContainerRef = useRef(null);
+   const { y: currentScrollY} = useWindowScroll();
+
+
+}
+
 
 const Nav = () => {
   return (
